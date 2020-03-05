b48facfaca55ba18ba0a1a3a0ef26ca2cd7fb72f
t5w0rd <lightning_0721@163.com>
2020-03-05 19:24:45 +0800

    hub, client, service

--- srv/websocket/client.go
+++ srv/websocket/client.go
@@ -2,16 +2,55 @@ package websocket
 
 import (
 	"github.com/gorilla/websocket"
+	log "github.com/micro/go-micro/v2/logger"
+	"time"
 )
 
-type Client struct {
+type client struct {
 	conn *websocket.Conn
+	id int64
+	send chan *Response
+	cycle time.Duration
 }
 
-func (c *Client) readPump() {
-
+func newClient(conn *websocket.Conn, id int64) *client {
+	c := &client{
+		conn: conn,
+		id: id,
+	}
+	return c
 }
 
-func (c *Client) writePump() {
+func (c *client) readPump() {
+	var req Response
+	for {
+		if err := c.conn.ReadJSON(&req); err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				// 意外的错误
+				log.Error(err)
+			}
+			break
+		}
+		switch req.Cmd {
+		default:
+			log.Errorf("Unsupported command: %v", req.Cmd)
+		}
+	}
+}
 
+func (c *client) writePump() {
+	ticker := time.NewTicker(c.cycle)
+	for {
+		select {
+		case <- ticker.C:
+			rsps := make([]*Response, 0, len(c.send) * 2)
+			for rsp := range c.send {
+				rsps = append(rsps, rsp)
+			}
+
+			if err := c.conn.WriteJSON(rsps); err != nil {
+				break
+			}
+		}
+	}
 }
--- srv/websocket/service.go
+++ srv/websocket/service.go
@@ -4,10 +4,12 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	log "github.com/micro/go-micro/v2/logger"
+	"time"
 )
 
 var (
 	Address string = "0.0.0.0:8080"
+	LoginDeadline time.Duration = time.Second * 1
 
 	upgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -15,12 +17,32 @@ var (
 	}
 )
 
+type Request struct {
+	Cmd string
+	Seq int64
+	Data interface{}
+}
+
+type Response struct {
+	Cmd string
+	Seq int64
+	Code int32
+	Data interface{}
+}
+
 type Service struct {
+	hb *hub
+	sendCycle time.Duration
+}
+
+type loginResult struct {
+	err error
+	uid int64
 }
 
 func NewService() *Service {
 	s := &Service{
-
+		hb: newHub(),
 	}
 	return s
 }
@@ -28,18 +50,71 @@ func NewService() *Service {
 func (s *Service) handleStream(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
 		return
 	}
-	client := &Client{
-		conn: conn,
+
+	result := make(chan *loginResult)
+	defer close(result)
+
+	go s.waitLogin(conn, result)
+
+	var id int64
+	ticker := time.NewTicker(LoginDeadline)
+	select {
+	case res := <- result:
+		if res.err != nil {
+			log.Error(res.err)
+			conn.Close()
+			return
+		}
+		id = res.uid
+	case <- ticker.C:
+		log.Info("Client hasnot logged in for a long time")
+		conn.Close()
+		<-result
+		return
 	}
-	go client.readPump()
-	go client.writePump()
+
+	cli := newClient(conn, id)
+	s.hb.register(cli)
+
+	go cli.readPump()
+	go cli.writePump()
+}
+
+// 等待客户端登陆
+func (s *Service) waitLogin(conn *websocket.Conn, result chan *loginResult) {
+	res := &loginResult{}
+
+	defer func() {
+		result <- res
+	}()
+
+	var req Request
+	if res.err = conn.ReadJSON(&req); res.err != nil {
+		return
+	}
+
+	rsp := &Response{
+		Cmd: req.Cmd,
+		Seq: req.Seq,
+	}
+	if res.err = conn.WriteJSON(rsp); res.err != nil {
+		return
+	}
+
+	res.uid = req.Data.(map[string]int64)["uid"]
 }
 
 func (s *Service) Run() error {
+	// 启动hub
+	go s.hb.run()
+
+	// 注册web服务处理器
 	http.HandleFunc("/stream", s.handleStream)
+
+	// 启动web服务
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe err: ", err)
